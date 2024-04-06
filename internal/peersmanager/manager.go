64da6b8a3561520736d54e7da81d537944f128b4
supermetrolog <billypro6@gmail.com>
2024-04-06 17:09:15 +0300

    developed peer manager

--- internal/peersmanager/manager.go
+++ internal/peersmanager/manager.go
@@ -1,26 +1,52 @@
 package peersmanager
 
 import (
+	"fmt"
 	"github.com/supermetrolog/myvpn/internal/protocol"
 	"net"
 )
 
 type PeersManager struct {
+	peers []*protocol.Peer
 }
 
 func New() *PeersManager {
 	return &PeersManager{}
 }
 
-func (p *PeersManager) Add(peer *protocol.Peer) error {
+func (pm *PeersManager) Add(peer *protocol.Peer) error {
+	pm.peers = append(pm.peers, peer)
+
 	return nil
 }
-func (p *PeersManager) Remove(peer *protocol.Peer) error {
-	return nil
+
+func (pm *PeersManager) Remove(peer *protocol.Peer) error {
+	for i, p := range pm.peers {
+		if peer == p {
+			pm.peers = append(pm.peers[i:], pm.peers[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("peer not found")
 }
-func (p *PeersManager) FindByDedicatedIp(ip net.IP) (peer *protocol.Peer, exists bool, err error) {
+
+func (pm *PeersManager) FindByDedicatedIp(ip net.IP) (peer *protocol.Peer, exists bool, err error) {
+	for _, peer := range pm.peers {
+		if peer.DedicatedIP().Equal(ip) {
+			return peer, true, nil
+		}
+	}
+
 	return nil, false, nil
 }
-func (p *PeersManager) FindByRealIp(ip net.IP) (peer *protocol.Peer, exists bool, err error) {
+
+func (pm *PeersManager) FindByRealIp(ip net.IP) (peer *protocol.Peer, exists bool, err error) {
+	for _, peer := range pm.peers {
+		if peer.RealIP().Equal(ip) {
+			return peer, true, nil
+		}
+	}
+
 	return nil, false, nil
 }
